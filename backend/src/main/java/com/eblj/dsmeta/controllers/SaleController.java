3341ecce2628d83eb3c7893574549b09c3389ddb
junior brandao <edilson_brandaojunior@hotmail.com>
2023-04-12 21:59:03 -0300

    Twilio SMS

--- backend/src/main/java/com/eblj/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/eblj/dsmeta/controllers/SaleController.java
@@ -1,6 +1,7 @@
 package com.eblj.dsmeta.controllers;
 
 import com.eblj.dsmeta.entities.Sale;
+import com.eblj.dsmeta.rest.SmsService;
 import com.eblj.dsmeta.rest.services.impl.SaleServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -13,11 +14,20 @@ public class SaleController {
 
     @Autowired
     private SaleServiceImpl service;
+
+    @Autowired
+    private SmsService smsService;
+
     @GetMapping
     public Page<Sale> findSales(
             @RequestParam(value = "minDate",defaultValue = "") String minDate,
             @RequestParam(value = "maxDate",defaultValue = "") String maxDate,
             Pageable pageable){
        return service.findSalles(minDate,maxDate,pageable);
+    }
+
+    @GetMapping("{id}/notification")
+    public void notifySms( @PathVariable Long id){
+      smsService.sendSms(id);
     }
 }
